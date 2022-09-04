e6e2edf4b7871a8c8cd34a2b813491fc8103ce8d
krithikaa22 <kuruthiga22@gmail.com>
2022-09-04 13:23:41 +0530

    fixed login mutation

--- src/components/MainHeader.js
+++ src/components/MainHeader.js
@@ -77,10 +77,10 @@ const MainHeader = ({user, setUser}) => {
                                             <button type="submit" className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" onClick={async () => {
                                                 setShowModal(false)
                                                  try{
-                                                    await login({variables: {email: Email, password: Password}})
+                                                    await loginMutation({variables: {email: Email, password: Password}})
                                                     if(login)
                                                     {
-                                                        setUser(True)
+                                                        setUser(true)
                                                     }
                                                     else{
                                                         window.alert(loginError)
